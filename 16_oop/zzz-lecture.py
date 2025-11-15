b9ac4ec413a10cecea84e75a49cb73ee5af55105
Ren Rahman <renaldhy@gmail.com>
2025-11-15 14:13:19 +0700

    init: boilerplate for task on D16

---
+++ 16_oop/zzz-lecture.py
@@ -0,0 +1,24 @@
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape('turtle')
+# timmy.color('coral')
+# timmy.forward(100)
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+pokemon = {
+  'Pokemon Name': ['Pikachu', 'Squirtle', 'Charmander'],
+  'Type': ['Electric', 'Water', 'Fire']
+}
+for key in pokemon:
+  table.add_column(key, pokemon[key])
+table.align = 'l'
+print(table)
