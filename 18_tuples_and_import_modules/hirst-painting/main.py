3d09dcb17432665e14d80451e0c96606a44020cc
Ren Rahman <renaldhy@gmail.com>
2025-11-16 16:25:37 +0700

    refactor: d18-challenge-1

--- 18_tuples_and_import_modules/hirst-painting/main.py
+++ 18_tuples_and_import_modules/hirst-painting/main.py
@@ -1,7 +1,17 @@
-import colorgram  # pyright: ignore[reportMissingImports]
+import colorgram as c  # pyright: ignore[reportMissingImports]
 
-colors = [
-    (color.rgb[0], color.rgb[1], color.rgb[2])
-    for color in colorgram.extract("image.jpg", 10)
-]
+
+def get_palette(img_path: str, n_colors: int):
+    """Return: list of rgb tuples ([ (9, 123, 46), ... ])
+
+    Dependecies: colorgram
+    """
+    return [
+        (color.rgb[0], color.rgb[1], color.rgb[2])
+        for color in c.extract(img_path, n_colors)
+    ]
+
+
+colors = get_palette("images.jpg", 30)
+
 print(colors)
---
+++ 18_tuples_and_import_modules/hirst-painting/readme.md
@@ -0,0 +1,1 @@
+image.jpg = https://i.guim.co.uk/img/static/sys-images/Arts/Arts_/Pictures/2012/1/13/1326458117290/Addictive---detail-of-Dam-007.jpg?width=465&dpr=1&s=none&crop=none
