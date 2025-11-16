f152675ee9471094d6106a05e69142b3af70b05a
Ren Rahman <renaldhy@gmail.com>
2025-11-16 16:35:49 +0700

    refactor: d18-challenge-1

---
+++ 18_tuples_and_import_modules/hirst-painting/helpers.py
@@ -0,0 +1,12 @@
+import colorgram as c  # pyright: ignore[reportMissingImports]
+
+
+def get_palette(img_path: str, n_colors: int):
+    """Return: list of rgb tuples ([ (9, 123, 46), ... ])
+
+    Dependecies: colorgram.py 1.2.0
+    """
+    return [
+        (color.rgb.r, color.rgb.g, color.rgb.b)
+        for color in c.extract(img_path, n_colors)
+    ]
--- 18_tuples_and_import_modules/hirst-painting/main.py
+++ 18_tuples_and_import_modules/hirst-painting/main.py
@@ -1,17 +1,5 @@
-import colorgram as c  # pyright: ignore[reportMissingImports]
+import helpers as h
 
-
-def get_palette(img_path: str, n_colors: int):
-    """Return: list of rgb tuples ([ (9, 123, 46), ... ])
-
-    Dependecies: colorgram
-    """
-    return [
-        (color.rgb[0], color.rgb[1], color.rgb[2])
-        for color in c.extract(img_path, n_colors)
-    ]
-
-
-colors = get_palette("images.jpg", 30)
+colors = h.get_palette("image.jpg", 30)
 
 print(colors)
