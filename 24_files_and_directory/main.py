246822120f8fa4e206ed0418013e09978415c779
Ren Rahman <renaldhy@gmail.com>
2025-11-26 19:10:25 +0700

    file system

--- 24_files_and_directory/main.py
+++ 24_files_and_directory/main.py
@@ -1,3 +1,26 @@
-from snake_game.main import app as snake_game
+# ########    File system   ########
+file_path = "my_file.txt"
+# ########    Read
+# # Open the file
+# my_file = open(file_path)
+# # Read the file
+# contents_1 = my_file.read()
+# # Print the file
+# print(contents_1)
+# # Close the file (Free up resource)
+# my_file.close()
 
-snake_game()
+# With keyword (no need to use .close())
+with open(file_path, mode="r") as file:
+    contents = file.read()
+    print(contents)
+
+# # ########    Write
+# # Replace (mode='w')
+# with open(file_path, mode="w") as f:
+#     contents = "New Text from main.py"
+#     f.write(contents)
+# # Append
+with open(file_path, mode="a") as f:
+    contents = "\nNew Text from main.py"
+    f.write(contents)
--- 24_files_and_directory/snake_game/helpers.py
+++
@@ -1,2 +0,0 @@
-def round_cor(cor: tuple):
-    return (round(cor[0], 2), round(cor[1], 2))
