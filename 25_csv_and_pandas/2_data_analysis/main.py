b98d5b5712797497d7248edaa5f71412d24f7bde
Ren Rahman <renaldhy@gmail.com>
2025-11-27 22:05:21 +0700

    d25: pandas practice

--- 25_csv_and_pandas/2_data_analysis/main.py
+++ 25_csv_and_pandas/2_data_analysis/main.py
@@ -1,6 +1,8 @@
-# Task: Create a csv which contain `Fur Color` and `Count`
+# Challenge: Create a csv which contain `Fur Color` and `Count`
 
-import pandas  # type: ignore
+# ############### My Solution
+
+import pandas as pd  # type: ignore
 
 path_csv = (
     "2_data_analysis/"
@@ -8,25 +10,72 @@ path_csv = (
 )
 path_result = "2_data_analysis/squirel_count.csv"
 
-df_squirrel = pandas.read_csv(path_csv)
+df_squirrel = pd.read_csv(path_csv)
 
-dict_color = {
-    "Fur Color": [],
-    "Count": [],
-}
+# dict_color = {
+#     "Fur Color": [],
+#     "Count": [],
+# }
 
-for i, row in df_squirrel.iterrows():
-    _color = row["Primary Fur Color"]
-    if not type(_color) is str:
-        continue
-    list_color = dict_color["Fur Color"]
-    list_count = dict_color["Count"]
-    if _color not in list_color:
-        list_color.append(_color)
-        list_count.append(0)
-    index_color = list_color.index(_color)
-    list_count[index_color] += 1
+# for i, row in df_squirrel.iterrows():
+#     _color = row["Primary Fur Color"]
+#     if not type(_color) is str:
+#         continue
+#     list_color = dict_color["Fur Color"]
+#     list_count = dict_color["Count"]
+#     if _color not in list_color:
+#         list_color.append(_color)
+#         list_count.append(0)
+#     index_color = list_color.index(_color)
+#     list_count[index_color] += 1
 
 
-df_result = pandas.DataFrame(dict_color)
-df_result.to_csv(path_result)
+# df_result = pandas.DataFrame(dict_color)
+# df_result.to_csv(path_result)
+
+
+# ############### Pythonic Solution
+
+
+# ######## Debug
+
+debug_result = df_squirrel["Primary Fur Color"].value_counts()
+# print(result)
+# CONSOLE OUTPUT:
+#
+# Primary Fur Color
+# Gray        2473
+# Cinnamon     392
+# Black        103
+# Name: count, dtype: int64
+#
+
+debug_result = debug_result.reset_index()
+# print(result)
+# CONSOLE OUTPUT:
+#
+#   Primary Fur Color  count
+# 0              Gray   2473
+# 1          Cinnamon    392
+# 2             Black    103
+#
+
+# print(result.columns)
+# CONSOLE OUTPUT:
+#
+# Index(['Primary Fur Color', 'count'], dtype='object')
+#
+
+debug_result.columns = ["Fur Color", "Count"]
+# print(debug_result.columns)
+# CONSOLE OUTPUT:
+#
+# Index(['Fur Color', 'Count'], dtype='object')
+#
+
+# ########
+
+alt_result = df_squirrel["Primary Fur Color"].value_counts().reset_index()
+alt_result.columns = ["Fur Color", "Count"]
+
+# alt_result.to_csv(path_result)
