6dd409061ea537672389021e5e3b551f3d4c4646
Ren Rahman <renaldhy@gmail.com>
2025-11-28 21:24:19 +0700

    d27: 0_lecture - **kwargs

--- 27_tkinkter_args_kwargs_and_create_gui_programs/0_lecture/sandbox.py
+++ 27_tkinkter_args_kwargs_and_create_gui_programs/0_lecture/sandbox.py
@@ -8,6 +8,34 @@ def add(*args: int | float):
         print("TypeError: args must be a number (integer or float)")
 
 
-print(add(1, 2, 3, 4, 5, 6, 7, 8, 9, ["asda"]))
+# print(add(1, 2, 3, 4, 5, 6, 7, 8, 9, ["asda"]))
 
-# ##################
+
+# ##################  **kwargs
+
+
+# kwargs = KeyWord ARGuments
+def calculate(n: int | float, **kwargs):
+    # for k, v in kwargs.items():
+    #     pass
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    return n
+
+
+# print(calculate(2, add=3, multiply=5))  # 25
+
+
+# Create Class with multiple optional arguments
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.color = kw.get("color")
+        self.seats = kw.get("seats")
+
+
+my_car = Car(make="Nissan")
+print(my_car.model)  # None
+
+# ##################  playground
