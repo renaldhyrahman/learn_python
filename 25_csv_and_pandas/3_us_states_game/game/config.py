d9c91cbc3ff29bcaa8477ca8b176828a7cd98e4e
Ren Rahman <renaldhy@gmail.com>
2025-11-28 14:42:50 +0700

    d25: refactor

--- 25_csv_and_pandas/3_us_states_game/game/config.py
+++ 25_csv_and_pandas/3_us_states_game/game/config.py
@@ -1,6 +1,18 @@
 PATH_IMG = "game/assets/blank_states_img.gif"
 PATH_CSV = "game/assets/50_states.csv"
+PATH_OUTPUT = "missed_state.csv"
+
 SCREEN_WIDTH = 725
 SCREEN_HEIGHT = 491
 
 GAME_TITLE = "US States Game"
+
+STATES_FONT = ("Courier", 8, "normal")
+STATES_ALIGN = "center"
+
+INFO_FONT = ("Courier", 36, "bold")
+INFO_ALIGN = "center"
+# INFO YCOR = MAX_Y - PADDING (10% of MAX_Y)
+INFO_COOR = (0, (SCREEN_HEIGHT / 2) - (0.1 * SCREEN_HEIGHT))
+
+EXIT_COMMANDS = ["Q", "Quit", "E", "Exit", "C", "Close"]
--- 25_csv_and_pandas/3_us_states_game/game/game.py
+++ 25_csv_and_pandas/3_us_states_game/game/game.py
@@ -9,9 +9,9 @@ class Game:
         self.df = pd.read_csv(gc.PATH_CSV)
         self.states = self.df.state.to_list()
         self.is_on = True
-        self.score = 0
-        self.user_past_guess = []
+        self.user_correct_guess = []
         self.setup_screen()
+        self.setup_writer()
 
     def setup_screen(self):
         self.screen = t.Screen()
@@ -19,42 +19,70 @@ class Game:
         self.screen.bgpic(gc.PATH_IMG)
         self.screen.title(gc.GAME_TITLE)
 
-    def write_state(self, user_guess: str):
-        state = t.Turtle(visible=False)
-        state.penup()
-        data = self.df[self.df.state == user_guess].iloc[0]
-        state.goto(data.x, data.y)
-        state.write(data.state, align="center", font=("Courier", 12, "normal"))
+    def setup_writer(self):
+        self.writer = t.Turtle(visible=False)
+        self.writer.penup()
+        self.writer.speed(0)
+
+    def write_state(
+        self, state: str, color: str = "black", is_saved: bool = False
+    ):
+        data = self.df[self.df.state == state].iloc[0]
+        coor = (data.x, data.y)
+        self.writer.goto(coor)
+        self.writer.color(color)
+        self.writer.write(
+            data.state, align=gc.STATES_ALIGN, font=gc.STATES_FONT
+        )
+        if is_saved:
+            loc_index = len(self.df_unguessed)
+            self.df_unguessed.loc[loc_index] = [data.state, data.x, data.y]
 
     def write_info(self, info: str):
-        writer = t.Turtle(visible=False)
-        writer.penup()
-        writer.color("red")
-        writer.write(info, align="center", font=("Courier", 36, "bold"))
+        self.writer.color("red")
+        self.writer.goto(gc.INFO_COOR)
+        self.writer.write(info, align=gc.INFO_ALIGN, font=gc.INFO_FONT)
 
     def game_over(self, is_player_win: bool = False):
         info = "You Win!" if is_player_win else "Game Over"
         self.write_info(info)
+        if not is_player_win:
+            self.write_missed_states()
+            self.df_unguessed.to_csv(gc.PATH_OUTPUT, index=False)
         self.is_on = False
         self.screen.exitonclick()
 
-    def logic_check_answer(self, user_guess: str):
-        if (user_guess not in self.states) or (
-            user_guess in self.user_past_guess
-        ):
-            return
-        self.user_past_guess.append(user_guess)
-        self.write_state(user_guess)
-        self.score += 1
+    def write_missed_states(self):
+        dict_unguessed = {
+            "states": [],
+            "x": [],
+            "y": [],
+        }
+        self.df_unguessed = pd.DataFrame(dict_unguessed)
+        for state in self.states:
+            if state not in self.user_correct_guess:
+                self.write_state(state=state, color="red", is_saved=True)
 
-    def play(self):
-        if self.score == len(self.states):
-            self.game_over(is_player_win=True)
-            return
-        title = f"{self.score}/50 States Correct"
-        prompt = "What's another state name?"
-        user_guess = self.screen.textinput(title=title, prompt=prompt)
-        if user_guess is None:
+    def logic_check_input(self, user_input: str):
+        if user_input in gc.EXIT_COMMANDS:
             self.game_over()
             return
-        self.logic_check_answer(user_guess.strip().title())
+        if user_input in self.user_correct_guess:
+            return
+        if user_input not in self.states:
+            return
+        self.user_correct_guess.append(user_input)
+        self.write_state(user_input)
+
+    def play(self):
+        score = len(self.user_correct_guess)
+        if score == len(self.states):
+            self.game_over(is_player_win=True)
+            return
+        title = f"{score}/50 States Correct"
+        prompt = "What's another state name?"
+        user_input = self.screen.textinput(title=title, prompt=prompt)
+        if user_input is None:
+            self.game_over()
+            return
+        self.logic_check_input(user_input.strip().title())
