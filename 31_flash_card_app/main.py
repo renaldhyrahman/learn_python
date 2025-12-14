feecde614ea50826b965edec09039681acdc9c19
Ren Rahman <renaldhy@gmail.com>
2025-12-14 23:22:11 +0700

    d31 - step 2

--- 31_flash_card_app/main.py
+++ 31_flash_card_app/main.py
@@ -1,12 +1,35 @@
+import random as r
 import tkinter as tk
+from dataclasses import dataclass
 
 import constants as cons
+import pandas
 from PIL import Image, ImageTk
 
 # ######################      State     ######################
 
 
-# ######################    Handlers    ######################
+@dataclass
+class State:
+    language: tk.StringVar
+    word: tk.StringVar
+
+
+# ######################     Helpers    ######################
+
+
+def fetch_df():
+    try:
+        df = pandas.read_csv(cons.Path.DATA.value)
+    except FileNotFoundError:
+        return
+    else:
+        languages = df.columns.to_list()
+        words = df.to_dict(orient="records")
+        return (languages, words)
+
+
+# ######################      Logic     ######################
 
 
 def display_card(is_front: bool):
@@ -17,12 +40,35 @@ def display_card(is_front: bool):
     canvas.itemconfig(c_img, image=image)
 
 
+def display_random_word():
+    languange, words = data
+
+    random_languange = r.choice(languange)
+    random_words = r.choice(words)
+
+    state.language.set(random_languange)
+    state.word.set(random_words[random_languange])
+
+
+# ######################    Handlers    ######################
+
+
 def on_known():
     print("on_known")
+    display_random_word()
 
 
 def on_unknown():
     print("on_unknown")
+    display_random_word()
+
+
+# ######################    Observer    ######################
+
+
+def obs_state_word(*args):
+    canvas.itemconfigure(c_text_language, text=state.language.get())
+    canvas.itemconfigure(c_text_word, text=state.word.get())
 
 
 # ######################    UI Setup    ######################
@@ -37,6 +83,16 @@ window.configure(
     pady=cons.Size.PADDING.value,
 )
 window.resizable(0, 0)
+
+# state and variables
+state = State(
+    language=tk.StringVar(),
+    word=tk.StringVar(),
+)
+data = None
+
+# attach observer/listener
+state.word.trace_add("write", obs_state_word)
 
 # images
 img_card_front = Image.open(cons.Path.IMG_CARD_FRONT.value)
@@ -60,16 +116,14 @@ canvas = tk.Canvas(
 c_img = canvas.create_image(cons.Coordinate.IMAGE.value)
 c_text_language = canvas.create_text(
     cons.Coordinate.LANGUAGE.value,
+    text=state.language.get(),
     font=cons.Font.LANGUAGE.value,
 )
 c_text_word = canvas.create_text(
     cons.Coordinate.WORD.value,
+    text=state.word.get(),
     font=cons.Font.WORD.value,
 )
-
-# canvas config
-canvas.itemconfig(c_text_language, text="French")
-canvas.itemconfig(c_text_word, text="trouve")
 
 # buttons
 button_known = tk.Button(
@@ -95,5 +149,13 @@ button_known.grid(column=1, row=1)
 
 # ######################     Start     ######################
 
-display_card(True)
-window.mainloop()
+try:
+    data = fetch_df()
+    if data is None:
+        raise FileNotFoundError
+except FileNotFoundError:
+    print("Data is not found.")
+else:
+    display_random_word()
+    display_card(True)
+    window.mainloop()
