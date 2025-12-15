f2df7c8a2d73c4a4fa53513fa77053b366737734
Ren Rahman <renaldhy@gmail.com>
2025-12-15 21:34:01 +0700

    d32 - init 2_birthday_wisher

--- 32_smtplib_datetime/2_birthday_wisher/constants.py
+++ 32_smtplib_datetime/2_birthday_wisher/constants.py
@@ -8,16 +8,7 @@ load_dotenv()
 
 
 class Smtp(Enum):
-    # 1
-    HOST_1 = getenv("SMTP_HOST_1")
-    PORT_1 = getenv("SMTP_PORT_1")
-    USER_1 = getenv("SMTP_USER_1")
-    PWD_1 = getenv("SMTP_PWD_1")
-    # 2
-    HOST_2 = getenv("SMTP_HOST_2")
-    PORT_2 = getenv("SMTP_PORT_2")
-    USER_2 = getenv("SMTP_USER_2")
-    PWD_2 = getenv("SMTP_PWD_2")
-
-
-PATH_QUOTES = "quotes.txt"
+    HOST = getenv("SMTP_HOST_1")
+    PORT = getenv("SMTP_PORT_1")
+    USER = getenv("SMTP_USER_1")
+    PWD = getenv("SMTP_PWD_1")
