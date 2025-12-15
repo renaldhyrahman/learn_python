5a1cb2920415d03a76e1ad9b06dbe8969b960b0a
Ren Rahman <renaldhy@gmail.com>
2025-12-16 03:46:02 +0700

    d33 - 1_kanye_quotes - challenge 1

--- 33_api_iss_overhead_notifier/1_kanye_quotes/main.py
+++ 33_api_iss_overhead_notifier/1_kanye_quotes/main.py
@@ -1,17 +1,31 @@
 import tkinter as tk
 
 import constants as cons
+import requests
 from PIL import Image, ImageTk
 
 
+def obs_quotes(*args):
+    canvas.itemconfig(canvas_text, text=quotes_var.get())
+
+
 def get_quote():
-    pass
-    # Write your code here.
+    res = requests.get(cons.API)
+    res.raise_for_status()
+    quote = res.json()["quote"]
+    quotes_var.set(quote)
 
 
+# window
 window = tk.Tk()
 window.title(cons.TITLE)
 window.config(padx=cons.PADDING, pady=cons.PADDING)
+
+# state
+quotes_var = tk.StringVar()
+
+# observer
+quotes_var.trace_add("write", obs_quotes)
 
 # images
 img_bg = Image.open(cons.IMG_BACKGROUND)
@@ -20,16 +34,14 @@ imgtk_bg = ImageTk.PhotoImage(img_bg)
 img_kanye = Image.open(cons.IMG_KANYE)
 imgtk_kanye = ImageTk.PhotoImage(img_kanye)
 
-
 # canvas
 canvas = tk.Canvas(width=img_bg_width, height=img_bg_height)
 img_xcor = img_bg_width / 2
 img_ycor = img_bg_height / 2
 canvas.create_image(img_xcor, img_ycor, image=imgtk_bg)
-text_quote = canvas.create_text(
+canvas_text = canvas.create_text(
     img_xcor,
     img_ycor,
-    text="Kanye Quote Goes HERE",
     width=cons.TEXT_WIDTH,
     font=cons.TEXT_FONT,
     fill=cons.TEXT_COLOR,
@@ -40,7 +52,7 @@ button_kanye = tk.Button(
     image=imgtk_kanye,
     highlightthickness=0,
     border=0,
-    command=None,
+    command=get_quote,
 )
 
 # layout
@@ -48,4 +60,5 @@ canvas.grid(column=0, row=0)
 button_kanye.grid(column=0, row=1)
 
 # run
+get_quote()
 window.mainloop()
