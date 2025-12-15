f30908076d4fcccfbb20eef610f25727e2a6133d
Ren Rahman <renaldhy@gmail.com>
2025-12-15 22:44:24 +0700

    d32 - birthday_wisher - step 2

--- 32_smtplib_datetime/2_birthday_wisher/constants.py
+++ 32_smtplib_datetime/2_birthday_wisher/constants.py
@@ -12,3 +12,8 @@ class Smtp(Enum):
     PORT = getenv("SMTP_PORT_1")
     USER = getenv("SMTP_USER_1")
     PWD = getenv("SMTP_PWD_1")
+
+
+class Path(Enum):
+    LETTERS_DIR = "assets/letter_templates"
+    DATA = "assets/birthdays.csv"
--- 32_smtplib_datetime/2_birthday_wisher/main.py
+++ 32_smtplib_datetime/2_birthday_wisher/main.py
@@ -10,3 +10,30 @@
 # 4. Send the letter generated in step 3 to that person's email address.
 
 # ######################  Solution  ######################
+
+
+import datetime as dt
+
+import constants as cons
+import pandas as pd
+
+# ######################    Func    ######################
+
+
+def fetch_data():
+    df_data = pd.read_csv(cons.Path.DATA.value)
+    return df_data.to_dict(orient="records")
+
+
+def send_mail(name: str, email: str):
+    print(f"{name} = {email}")
+
+
+# ######################    Run     ######################
+
+
+list_data = fetch_data()
+today = dt.date.today()
+for data in list_data:
+    if data["month"] == today.month and data["day"] == today.day:
+        send_mail(data["name"], data["email"])
