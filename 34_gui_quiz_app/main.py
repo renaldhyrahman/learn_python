bf5e31cdd10a2f6d1106069b474cf5cbedc9600e
Ren Rahman <renaldhy@gmail.com>
2025-12-16 17:05:19 +0700

    d34 - challenge 1 - refactor

--- 34_gui_quiz_app/main.py
+++ 34_gui_quiz_app/main.py
@@ -1,19 +1,3 @@
-from app.data import question_data
-from app.question_model import Question
-from app.quiz_brain import QuizBrain
+from app.main import start
 
-question_bank = []
-for question in question_data:
-    question_text = question["question"]
-    question_answer = question["correct_answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
-
-
-quiz = QuizBrain(question_bank)
-
-while quiz.still_has_questions():
-    quiz.next_question()
-
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+start()
